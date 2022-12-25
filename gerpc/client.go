e359932e25a6d0f2416436f4d105bf4e537ecc9a
Kallen <keleix@gmail.com>
2022-12-25 20:06:19 +0800

    feat: remote client

---
+++ gerpc/client.go
@@ -0,0 +1,52 @@
+package gerpc
+
+import (
+	"errors"
+	"io"
+	"sync"
+
+	"geek/gerpc/codec"
+)
+
+// Call stands for an active RPC
+type Call struct {
+	Seq           uint64
+	ServiceMethod string
+	Args          interface{}
+	Reply         interface{}
+	Error         error
+	Done          chan *Call
+}
+
+func (call *Call) done() {
+	call.Done <- call
+}
+
+// Client stands for a RPC client
+type Client struct {
+	cc       codec.Codec
+	opt      *Option
+	sending  sync.Mutex
+	header   codec.Header
+	mu       sync.Mutex
+	seq      uint64
+	pending  map[uint64]*Call
+	closing  bool
+	shutdown bool
+}
+
+var _ io.Closer = (*Client)(nil)
+
+// ErrShutdown client shutdown error
+var ErrShutdown = errors.New("connection on closing")
+
+// Close the connection
+func (cli *Client) Close() error {
+	cli.mu.Lock()
+	defer cli.mu.Unlock()
+	if cli.closing {
+		return ErrShutdown
+	}
+	cli.closing = true
+	return cli.cc.Close()
+}
